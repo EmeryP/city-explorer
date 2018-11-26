28fce3fb8c8563abf8cd5608f84a17f22b054f61
emeryparks <emeryparks09@hotmail.com>
2018-11-25 19:49:22 -0800

    hitting all apis, rough styling completed

--- src/App.js
+++ src/App.js
@@ -15,11 +15,18 @@ class App extends Component {
       locationUrl: '',
       latitude: 0,
       longitude: 0,
-      records: [],
       JSON: '',
       formattedQuery: '',
       WEATHER_API: '',
-      weatherLogs: []
+      weatherLogs: [],
+      YELP_API: '',
+      yelpLogs: [],
+      MOVIES_API: '',
+      moviesLogs: [],
+      MEETUPS_API: '',
+      meetupsLogs: [],
+      TRAILS_API: '',
+      trailsLogs: [],
     }
   }
 
@@ -32,13 +39,51 @@ class App extends Component {
     console.log(this.state);
   }
 
-  hitApis = () => {
+  hitWeatherApi = () => {
     superagent('get', `${this.state.WEATHER_API}`)
       .then(results => {
         this.setState({
-          weatherLogs: results.text,
-          records: results.body,
-          
+          weatherLogs: results.body,          
+        })
+      })
+      .catch(console.error);
+  }
+
+  hitYelpApi = () => {
+    superagent('get', `${this.state.YELP_API}`)
+      .then(results => {
+        this.setState({
+          yelpLogs: results.body,          
+        })
+      })
+      .catch(console.error);
+  }
+
+  hitMeetupsApi = () => {
+    superagent('get', `${this.state.MEETUPS_API}`)
+      .then(results => {
+        this.setState({
+          meetupsLogs: results.body,          
+        })
+      })
+      .catch(console.error);
+  }
+
+  hitMoviesApi = () => {
+    superagent('get', `${this.state.MOVIES_API}`)
+      .then(results => {
+        this.setState({
+          moviesLogs: results.body,          
+        })
+      })
+      .catch(console.error);
+  }
+
+  hitTrailsApi = () => {
+    superagent('get', `${this.state.TRAILS_API}`)
+      .then(results => {
+        this.setState({
+          trailsLogs: results.body,          
         })
       })
       .catch(console.error);
@@ -54,10 +99,23 @@ class App extends Component {
         JSON: results.text,
         formattedQuery: results.body.formatted_query,
         MAPS_API: `https://maps.googleapis.com/maps/api/staticmap?center=${results.body.latitude}%2c%20${results.body.longitude}&zoom=13&size=600x300&maptype=roadmap%20%20&key=AIzaSyDp0Caae9rkHUHwERAFzs6WN4_MuphTimk`,
-        WEATHER_API: `https://city-explorer-backend.herokuapp.com/weather?data%5Bid%5D=${results.body.id}&data%5Bsearch_query%5D=${results.body.search_query}&data%5Bformatted_query%5D=${results.body.formatted_query}&data%5Blatitude%5D=${results.body.latitude}&data%5Blongitude%5D=${results.body.longitude}&data%5Bcreated_at%5D=`
+
+        WEATHER_API: `https://city-explorer-backend.herokuapp.com/weather?data%5Bid%5D=${results.body.id}&data%5Bsearch_query%5D=${results.body.search_query}&data%5Bformatted_query%5D=${results.body.formatted_query}&data%5Blatitude%5D=${results.body.latitude}&data%5Blongitude%5D=${results.body.longitude}&data%5Bcreated_at%5D=`,
+
+        YELP_API: `https://city-explorer-backend.herokuapp.com/yelp?data%5Bid%5D=${results.body.id}&data%5Bsearch_query%5D=${results.body.search_query}&data%5Bformatted_query%5D=${results.body.formatted_query}&data%5Blatitude%5D=${results.body.latitude}&data%5Blongitude%5D=${results.body.longitude}&data%5Bcreated_at%5D=`,
+
+        MEETUPS_API: `https://city-explorer-backend.herokuapp.com/meetups?data%5Bid%5D=${results.body.id}&data%5Bsearch_query%5D=${results.body.search_query}&data%5Bformatted_query%5D=${results.body.formatted_query}&data%5Blatitude%5D=${results.body.latitude}&data%5Blongitude%5D=${results.body.longitude}&data%5Bcreated_at%5D=`,
+
+        MOVIES_API: `https://city-explorer-backend.herokuapp.com/movies?data%5Bid%5D=${results.body.id}&data%5Bsearch_query%5D=${results.body.search_query}&data%5Bformatted_query%5D=${results.body.formatted_query}&data%5Blatitude%5D=${results.body.latitude}&data%5Blongitude%5D=${results.body.longitude}&data%5Bcreated_at%5D=`,
+    
+        TRAILS_API: `https://city-explorer-backend.herokuapp.com/trails?data%5Bid%5D=${results.body.id}&data%5Bsearch_query%5D=${results.body.search_query}&data%5Bformatted_query%5D=${results.body.formatted_query}&data%5Blatitude%5D=${results.body.latitude}&data%5Blongitude%5D=${results.body.longitude}&data%5Bcreated_at%5D=`
         
         })
-        this.hitApis()
+        this.hitWeatherApi()
+        this.hitYelpApi()
+        this.hitMeetupsApi()
+        this.hitMoviesApi()
+        this.hitTrailsApi()
       })
       .catch(console.error);
   }
@@ -78,15 +136,79 @@ class App extends Component {
           <img id='map' src={this.state.MAPS_API} alt='Maps_image'/>
 
           <h3>Here are your results for {this.state.formattedQuery}</h3>
-          <section className='column-container'>
+          <div className='column-container'>
+          <section>
+          <h3>Results from the Dark Sky API</h3>
           <ul>
             {
-              this.state.records.map((record, index) => {
+              this.state.weatherLogs.map((record, index) => {
                 return <li key={index}>The forecast for {record.time} is: {record.forecast}</li> 
               })
             }
           </ul>
           </section>
+
+          <section>
+          <h3>Results from the Yelp API</h3>
+          <ul>
+            {
+              this.state.yelpLogs.map((record, index) => {
+                return <li key={index}>
+                <a href={record.url}>{record.name}</a>
+                <p>The average rating is {record.rating} out of 5 and the average cost is {record.price} out of $$$$</p>
+                <img src={record.image_url} alt='url img'/>
+              </li> 
+              })
+            }
+          </ul>
+          </section>
+
+          <section>
+          <h3>Results from the Meetups API</h3>
+          <ul>
+            {
+              this.state.meetupsLogs.map((record, index) => {
+                return <li key={index}>
+                <a href={record.link}>{record.name}</a>
+                <p>Hosted by: {record.host}</p>
+                <p>Created on: {record.creation_date}</p>
+              </li> 
+              })
+            }
+          </ul>
+          </section>
+
+          <section>
+            <h3>Results from the Movie DB API</h3>
+          <ul>
+            {
+              this.state.moviesLogs.map((record, index) => {
+                return <li key={index}>
+                <p><span>{record.title}</span> was released on {record.released_on}. Out of {record.total_votes}, {record.title} has an average vote of {record.average_votes} and a popularity score of {record.popularity}.</p>
+                <img src={record.image_url} alt='url img'/>
+                <p>{record.overview}</p>
+              </li> 
+              })
+            }
+          </ul>
+          </section>
+
+          <section>
+          <h3>Results from the Hiking Project API</h3>
+          <ul>
+            {
+              this.state.trailsLogs.map((record, index) => {
+                return <li key={index}>
+                <p>Hike Name: <a href={record.trail_url}>{record.name}</a>, Location: {record.location}, Distance: {record.length} miles</p>
+                <p>On {record.condition_date} at {record.condition_time} trail conditions were reported as {record.conditions}</p>
+                <p>This trail has a rating of {record.stars} stars (out of {record.star_votes} votes)</p>
+                <p>{record.summary}</p>
+              </li> 
+              })
+            }
+          </ul>
+          </section>
+        </div>
         </section>
       </React.Fragment>
     );
